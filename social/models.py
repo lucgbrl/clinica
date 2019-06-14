61cfbc8f9e8de443ea078d72f98168611f889efa
lucgrl <lucassantos@alu.ufc.br>
2019-06-14 15:54:47 -0300

    Add post

--- social/models.py
+++ social/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(verbose_name="Titulo", max_length = 256)
+
+    def __str__(self):
+        return self.title
